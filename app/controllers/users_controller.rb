11edaad8802cab445eeb15981c7774c1d2a43330
kab <kab3nan9@gmail.com>
2019-04-21 01:07:12 +0900

    ルーティングの設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,17 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = Hash.new
+    if params[:username] == 'InfoItomiku'
+      @user[:name] = 'Miku Ito'
+      @user[:username] = '@InfoItomiku'
+      @user[:location] = 'Tokyo,Japan'
+      @user[:about] = 'Nice to meet you.'
+    elsif params[:username] == 'kab_nan'
+      @user[:name] = 'kab'
+      @user[:username] = '@kab_nan'
+      @user[:location] = 'Hokkaido,Japan'
+      @user[:about] = 'Hello World'
+    end
   end
 end
